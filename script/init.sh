f2c235992f9829348e8472ae2b157189775b3ced
wuming <1191552161@qq.com>
2020-10-31 08:37:44 +0800

    f

---
+++ script/init.sh
@@ -0,0 +1,8 @@
+#交换ctrl 和 capslock
+setxkbmap -option "ctrl:swapcaps"
+
+# 自动代理 ssh
+ssh-add ~/.ssh/coding_rsa
+ssh-add ~/.ssh/github_rsa
+
+xrandr --output LVDS-1-1 --primary --output VGA-1-1 --right-of LVDS-1-1
