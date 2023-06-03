c56feadf333b66ee3c4a9da120b7b6612369d38a
hanyudeye <hanyudeye@163.com>
2023-06-03 13:59:19 +0800

    w

--- script/aoc.sh
+++
@@ -1,3 +0,0 @@
-# aoc 单用
-xrandr --output DP-0 --auto  --mode 2560x1440  --primary --output HDMI-0 --off
-
--- script/setdis.sh
+++
@@ -1,9 +0,0 @@
-# eg: bash /orb/Dev/BashScripts/setres.sh 200 125 
-# Set multimonitor with multi-dpi with xrandr (eg NVidia cards)
-# First set the scaling to something at least large enough (s.a. 200% in GNOME)
-# In this case DVI-D-0 and HDMI-0 are 1080p monitors around DP-0 which is a 4K monitor
-
-SCALE1=$(expr $1/100 | bc -l)
-SCALE2=$(expr $2/100 | bc -l)
-
-xrandr --output DP-0 --mode 1920x1080 --panning $((1920*$2/100))x$((1080*$2/100))+$((1920*$2/100+3840*$1/100))+0 --pos 3840x0 --rotate right --scale ${SCALE2}x${SCALE2} --output HDMI-0 --primary --mode 3840x2160 --panning $((3840*$1/100))x$((1080*$1/100))+$((1920*$2/100))+0 --pos 0x0 --rotate normal --scale ${SCALE1}x${SCALE1}
